3fe47015456e9e33bc4be9e361fd672f3c04e4a4
Extra0703 <extra900703@gmail.com>
2022-06-14 18:00:23 +0800

    Question1 done.

--- src/Question1/Question1.tsx
+++ src/Question1/Question1.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import '../Sass/Question1.css'
 
 const Question1 = () => {
-  return <div className="div">Question1</div>
+  const [count, setCount] = useState<number>(0)
+  const handleCountChange = () => {
+    setCount((prev) => prev + 1)
+  }
+
+  return (
+    <div className="question1">
+      <button className="button" onClick={handleCountChange}>
+        Click Me
+      </button>
+      <p className="badge">
+        Count: <span>{count}</span>
+      </p>
+    </div>
+  )
 }
 
 export default Question1
